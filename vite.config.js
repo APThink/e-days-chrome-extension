411ebd4ec27bff2b833fc337f7f16349fdfbc242
Alex Pinneker <pinneker7@gmail.com>
2026-01-11 23:11:36 +0100

    add icons

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { copyFileSync, writeFileSync, mkdirSync, existsSync } from 'fs';
+import { copyFileSync, writeFileSync, mkdirSync, existsSync, readdirSync } from 'fs';
 
 export default defineConfig({
   build: {
@@ -28,6 +28,16 @@ export default defineConfig({
         copyFileSync('src/popup.html', 'dist/popup.html');
         copyFileSync('src/manifest.json', 'dist/manifest.json');
         
+        if (existsSync('src/icons')) {
+          if (!existsSync('dist/icons')) {
+            mkdirSync('dist/icons');
+          }
+          const files = readdirSync('src/icons');
+          files.forEach(file => {
+            copyFileSync(`src/icons/${file}`, `dist/icons/${file}`);
+          });
+        }
+
         if (existsSync('src/config.js')) {
           copyFileSync('src/config.js', 'dist/config.js');
         } else if (!existsSync('dist/config.js')) {
